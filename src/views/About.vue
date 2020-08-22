d225443bdb22a83d12d8c754a80e62fb5875ad05
hcl242 <1216096951@qq.com>
2020-08-22 14:38:40 +0800

    a

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+	<h2>{{msg}}</h2>
+	<h4>{{ahello}}</h4>
+	<button @click="change()">切换</button>
   </div>
 </template>
+
+<script>
+	import {ref,toRefs, reactive} from 'vue'
+	export default {
+		setup(){
+			
+			const ahello = ref('ahello');
+			
+			const state = reactive({ 
+				msg:'hello'
+			})
+			const change = () => {
+				state.msg = 'hi'
+			}
+			return {ahello,...toRefs(state),change}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
